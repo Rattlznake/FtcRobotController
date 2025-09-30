8421739d2de7b89bcd521d39104359894a26e7a7
nate <nunya@example.com>
2025-09-30 16:04:22 -0400

    Create constructor for Drivetrain class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -1,8 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Drivetrain {
-    DcMotor LeftMotor;
-    DcMotor RightMotor;
+    HardwareMap hardwareMap;
+
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+
+    double rightMotorModifier = 1;
+    double leftMotorModifier = 1;
+
+    Drivetrain(HardwareMap hardwareMap) {
+        this.hardwareMap = hardwareMap;
+
+        leftMotor = hardwareMap.get(DcMotor.class, "LeftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "RightMotor");
+    }
+
+    void setWheelPower(double leftWheel, double rightWheel) {
+        leftMotor.setPower(leftWheel * rightMotorModifier);
+        rightMotor.setPower(rightWheel * rightMotorModifier);
+    }
+
+
 }
